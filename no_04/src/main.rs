6777d623c8e2f947543d13c602f29ba4578f362b
teki.sei <teki.sei@uzabase.com>
2019-05-28 04:51:10 +0900

    No_04のソースコード

---
+++ no_04/src/main.rs
@@ -0,0 +1,95 @@
+#![allow(warnings)]
+
+fn main() {
+    let gender = Gender::Male;
+    match gender {
+        Gender::Male => println!("a man"),
+        Gender::Female => println!("a woman"),
+    }
+    match gender {
+        Gender::Male => println!("a man"),
+        _ => println!("not a man")
+    }
+
+    let ip = IpAddr::V4(192, 168, 0, 1);
+    match ip {
+        IpAddr::V4(i1, i2, i3, i4) => println!("v4: {}.{}.{}.{}", i1, i2, i3, i4),
+        IpAddr::V6(addr) => println!("v6: {}", addr)
+    }
+
+    match generate() {
+        Some(string) => println!("{}", string),
+        None => println!("nothing")
+    }
+
+    let str1 = generate();
+    let str2 = generate();
+
+    match (&str1, &str2) {
+        (Some(s1), Some(s2)) => println!("both str1({}), str2({}) are exist", s1, s2),
+        (Some(s1), None) => println!("only str1({}) is exist", s1),
+        (None, Some(s2)) => println!("only str2({}) is exist", s2),
+        (None, None) => println!("both str1, str2 are nothing"),
+    }
+
+    match (&str1, &str2) {
+        (Some(s1), Some(s2)) => println!("both str1({}), str2({}) are exist", s1, s2),
+        (None, None) => println!("both str1, str2 are nothing"),
+        _ => println!("what ever, I don't care")
+    }
+
+    let result = if generate().is_some() { "has data" } else { "has no data" };
+
+    if let Some(string) = generate() {
+        println!("has data: {}", string);
+    } else {
+        println!("has no data");
+    }
+
+//    panic!("oh my god");
+
+    println!("this will never be printed");
+
+    match parse("123") {
+        Ok(int) => println!("i32 parse result: {}", int),
+        Err(ErrorWrapper::ParseI32Failed(error)) => println!("{}", error)
+    }
+
+    match parse_and_double("123") {
+        Ok(int) => println!("i32 parse and doubled result: {}", int),
+        Err(ErrorWrapper::ParseI32Failed(error)) => println!("{}", error)
+    }
+}
+
+fn generate() -> Option<String> {
+    Option::Some("test".into())
+////    Option::None
+//    unimplemented!()
+}
+
+fn parse(string: &str) -> std::result::Result<i32, ErrorWrapper> {
+    match string.parse::<i32>() {
+        Ok(int) => Ok(int),
+        Err(error) => Err(ErrorWrapper::ParseI32Failed(
+            format!("i32 ({}) parse error: {}", string, error)))
+    }
+}
+
+fn parse_and_double(string: &str) -> std::result::Result<i32, ErrorWrapper> {
+    let int = parse(string)?;
+    Ok(int * 2)
+}
+
+enum Gender {
+    Male,
+    Female,
+}
+
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum ErrorWrapper {
+    ParseI32Failed(String)
+}
