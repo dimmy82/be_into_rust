2d26df971834e0c03f0714732d0b1cd67f3f0fdb
teki.sei <teki.sei@uzabase.com>
2019-09-09 10:39:47 +0900

    update question in no_01

--- no_01/src/question.rs
+++ no_01/src/question.rs
@@ -1,30 +1,101 @@
-fn q1() {
-    let s1 = String::from("q1s1");
-//    let s2 = s1; // この行を修正してコンパイルできるようにする
-//    println!("{}", s1);
-//    println!("{}", s2);
-}
-
-fn q2() {
-    let s1 = String::from("q1s1");
-//  s1 を受け取る q1_func1 関数を作ってここで呼び出す
-    println!("{}", s1);
-}
-
-fn q3() {
-    let o = Q3Object {
-        a: String::from("aaa"),
-        b: String::from("bbb"),
-    };
-    // q3_func1 関数を使って o.a, o.b を出力する
-}
-
-fn q3_func1(s: String) {
-    println!("{}", s);
-}
-
-struct Q3Object {
+#[derive(Debug)]
+struct Object1 {
     pub a: String,
     pub b: String,
 }
 
+#[derive(Debug)]
+struct Object2 {
+    a: String,
+}
+
+impl Object2 {
+    fn new(a: String) -> Self {
+        Object2 { a }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////
+
+pub fn q1() {
+    let s1 = String::from("q1s1");
+    //    let s2 = s1; // この行を修正してコンパイルできるようにする
+    //    println!("{}", s1);
+    //    println!("{}", s2);
+}
+
+///////////////////////////////////////////////////////////////////////
+
+pub fn q2() {
+    let s1 = String::from("q1s1");
+    //  s1 を受け取る q1_func1 関数を作ってここで呼び出す
+    println!("{}", s1);
+}
+
+///////////////////////////////////////////////////////////////////////
+
+pub fn q3() {
+    let o = Object1 {
+        a: String::from("aaa"),
+        b: String::from("bbb"),
+    };
+    // q3_func1, q3_func2 関数を使って o.a, o.b を出力する
+}
+
+fn q3_func1(o: Object1) {
+    println!("{}", o.a);
+}
+
+fn q3_func2(o: Object1) {
+    println!("{}", o.b);
+}
+
+///////////////////////////////////////////////////////////////////////
+
+pub fn q4() {
+    let o = Object1 {
+        a: String::from("aaa"),
+        b: String::from("bbb"),
+    };
+    // q4_func1 関数を使って o.a, o.b を出力する
+}
+
+fn q4_func1(s: String) {
+    println!("{}", s);
+}
+
+///////////////////////////////////////////////////////////////////////
+
+pub fn q5() {
+    let o = Object2::new(String::from("aaa"));
+    //  Object2 に q5_func1 というメソッドを作って Object2.a を返すようにして、ここで println をする
+    println!("{:?}", o)
+}
+
+impl Object2 {
+    // q5_func1 のメソッドはここに実装する
+}
+
+///////////////////////////////////////////////////////////////////////
+
+pub fn q6() {
+    // Object2::new でインスタンスを作成する
+    // Object2 に q6_func1 というメソッドを作って Object2.a を変更する
+    // （&str の引数を受け取って、 Object.a の後ろにくっ付ける）
+    // Object2.q5_func1 を呼び出して、ここで println をする
+}
+
+impl Object2 {
+    // q6_func1 のメソッドはここに実装する
+}
+
+///////////////////////////////////////////////////////////////////////
+
+pub fn q7() {
+    // Object2::new でインスタンスを作成する
+    // Object2 に q7_func1 というメソッドを作って、新しい Object2 インスタンスを返すし、Object2.a の値を移譲する（clone()は使わない）
+}
+
+impl Object2 {
+    // q7_func1 のメソッドはここに実装する
+}
